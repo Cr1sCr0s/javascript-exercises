1fe2685918daf22e53117f31dfcb4b9f5c24d492
cris <crisrelayo17@gmail.com>
2026-06-22 13:15:31 +0800

    complete exercise 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let newArray = array.slice();
+  for(let i=0; i<newArray.length; i++){
+    for(const arg of args){
+      if(newArray[i] === arg){
+        newArray.splice(i, 1);
+        i--;
+      }
+    }
+  }
+  return newArray;
 };
+// removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
