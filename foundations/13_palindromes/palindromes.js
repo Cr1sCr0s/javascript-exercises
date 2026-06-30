3b93994e0892b59f15cdf962966772b2799cd078
cris <crisrelayo17@gmail.com>
2026-06-30 17:24:35 +0800

    Complete exercise 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase();
 
+  const stringSplit = string
+  .split('')
+  .filter(char => (/[a-z]|[0-9]/).test(char));
+
+  const halfLength = Math.floor(stringSplit.length / 2);
+
+  for(let i=0; i<halfLength; i++){
+    if(
+      !(stringSplit.at(i) === stringSplit.at(-(i+1)))
+    ){ return false; }
+  }
+  return true;
 };
 
 // Do not edit below this line
