65fa4ec0384481b1e72b85958eff4dab3c5ac99a
cris <crisrelayo17@gmail.com>
2026-06-30 19:01:42 +0800

    Complete exercise 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    if(!oldest.yearOfDeath) 
+      oldest.yearOfDeath = (new Date()).getFullYear();
+    if(!person.yearOfDeath) 
+      person.yearOfDeath = (new Date()).getFullYear();
+    const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    const personAge = person.yearOfDeath - person.yearOfBirth;
+    return (oldestAge > personAge) ? oldest : person;
+  });
 };
 
 // Do not edit below this line
