988bb2099b63fec77bc8776c1767c343d0e49a9e
cris <crisrelayo17@gmail.com>
2026-06-30 18:11:27 +0800

    Complete exercise 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(target) {
+  target = +(target);
+  if(target < 0) return 'OOPS';
+  if(target === 0) return 0;
+  let curr = 1; let prev = 0; let temp = 0;
+  for(let i = 1; i<target; i++){
+    temp = curr;
+    curr += prev;
+    prev = temp;
+  }
+  return curr;
 };
 
 // Do not edit below this line
